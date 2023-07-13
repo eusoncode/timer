fd9b9d601987b65b6425e64f4c1b26fb361b9d07
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-13 17:26:41 -0600

    Simple Timer

---
+++ timer1.js
@@ -0,0 +1,16 @@
+const args = process.argv.slice(2);
+let delay = 1000;
+
+for (const arg of args) {
+  let secBeep = Number(arg);  
+
+  if (Number.isNaN(arg) || secBeep < 0) {
+    console.log(`invalide: ${arg}`);
+    continue;
+  }
+
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, delay);
+  delay += (secBeep * 1000);
+}
